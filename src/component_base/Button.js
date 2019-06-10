0610405e832d38f296d4ea832a176373d7eb9425
nakapol <nakapolpormmakoon@gmail.com>
2019-06-10 16:02:28 +0700

    :art: change base color :motorcycle:

--- src/component_base/Button.js
+++ src/component_base/Button.js
@@ -5,5 +5,9 @@ export const Button = styled.button`
   border-radius: 3px;
   padding: 8px 14px;
   outline: none;
-  background-color: #ffffff;
+  background-color: #f7f7f8;
+
+  @media only screen and (max-width: 767px) {
+    background-color: #ffffff;
+  }
 `
--- src/component_base/Input.js
+++ src/component_base/Input.js
@@ -5,6 +5,11 @@ export const Input = styled.input`
   border-radius: 3px;
   padding: 6px;
   outline: none;
+  background-color: #f7f7f8;
 
   color: #3a3a3a;
+
+  @media only screen and (max-width: 767px) {
+    background-color: #ffffff;
+  }
 `
